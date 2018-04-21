f399296a55271f409181ace7ecbe93457844a7bf
jonathonengelien <jonathon.engelien@gmail.com>
2018-04-21 07:32:51 -0500

    Got it to display one gif...but not delete the old ones

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     //Variables
 
@@ -11,7 +11,7 @@ $(document).ready(function() {
     ////////////////////////////// Functions ///////////////////////////////
 
 
-  // Display Buttons
+    // Display Buttons
     function renderButtons() {
         $("#buttons-appear-here").empty();
         console.log("I'm working!");
@@ -25,34 +25,59 @@ $(document).ready(function() {
         }
     };
 
-    // When a button is clicked
-    $("#add-emotion").on("click", function(event) {
+    // When a new button is added
+    $("#add-emotion").on("click", function (event) {
         event.preventDefault();
         var emotion = $("#emotion-input").val().trim();
         buttonTitles.push(emotion);
         renderButtons();
     });
 
-    // Re-render the HTML to display the appropriate content (API Code)
+    // Render the HTML to display the appropriate content (API Code)
     function displayGifs() {
         var emotion = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + emotion + "&api_key=Z7UtmkF42WDI9goZAOmkReUnPqsptynl&limit=20&rating=pg";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + emotion + "&api_key=Z7UtmkF42WDI9goZAOmkReUnPqsptynl&limit=20";
 
         //AJAX Call
         $.ajax({
             url: queryURL,
             method: "GET"
-          }).then(function(response) {
+        }).then(function (response) {
+            //First emptys the div container
+            //$("#gifs-appear-here").empty();
+            //Variable to hold the response data
+            var results = response.data;
 
-            console.log(queryURL);
-          });
+            for (var i = 0; i < results.length; i++) {
+                //Creating the div to hold the gif information
+                var gifDiv = $("<div class='gif'>");
+
+                //Displaying the Rating of the gif
+                var p = $("<p>").text("Rating: " + results[i].rating);
+
+                //Displaying the Image of the gif
+                var gifImage = $("<img>");
+                gifImage.addClass("image-fluid");
+                gifImage.attr("src", results[i].images.fixed_height.url);
+
+                // Appending the paragraph and image tag to the animalDiv
+                gifDiv.append(p);
+                gifDiv.append(gifImage);
+            };
+
+            $("#gifs-appear-here").prepend(gifDiv);
+        });
     };
 
-    
+
     ////////////////////////////// Working Code ///////////////////////////////
 
     // Adding a click event listener to all elements with a class of "emotion-btn"
     //$(document).on("click", ".emotion-btn", displayGifs);
+
+    // Adding a click event listener to all elements with a class of "emotion-btn"
+    $(document).on("click", ".emotion-btn", displayGifs);
+
 
     // Calling the renderButtons function to display the intial buttons
     renderButtons();
